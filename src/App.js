14b43a04b529eec0af50dbbb2973719548d49c98
kumarabhishekbansal <kumarabhishekbansal>
2024-03-27 21:16:06 +0530

    change the name of chunks using webpackchunkname

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import styled, { createGlobalStyle } from "styled-components";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import AppContext from "./context/AppContext";
-const Lists = lazy(() => import("./pages/Lists"));
-const ListDetail = lazy(() => import("./pages/ListDetail"));
-const ListForm = lazy(() => import("./pages/ListForm"));
+const Lists = lazy(() =>
+  import(
+    /* webpackChunkName:
+ "Lists" */ "./pages/Lists"
+  )
+);
+const ListDetail = lazy(() =>
+  import(
+    /* webpackChunkName:
+ "ListDetail" */ "./pages/ListDetail"
+  )
+);
+const ListForm = lazy(() =>
+  import(
+    /* webpackChunkName: 
+ "ListForm" */ "./pages/ListForm"
+  )
+);
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
@@ -30,13 +45,13 @@ function App() {
         <BrowserRouter>
           <Header />
           <Suspense fallback={<div>Loading...</div>}>
-          <AppContext>
-            <Routes>
-              <Route path="/" element={<Lists />} />
-              <Route path="/list/:listId/new" element={<ListForm />} />
-              <Route path="/list/:listId" element={<ListDetail />} />
-            </Routes>
-          </AppContext>
+            <AppContext>
+              <Routes>
+                <Route path="/" element={<Lists />} />
+                <Route path="/list/:listId/new" element={<ListForm />} />
+                <Route path="/list/:listId" element={<ListDetail />} />
+              </Routes>
+            </AppContext>
           </Suspense>
         </BrowserRouter>
       </AppWrapper>
